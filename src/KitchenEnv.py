b0384d6b340070c2093964006edf82d31add629d
goelshivam1210 <goelshivam1210@gmail.com>
2023-08-10 01:38:27 -0400

    working training SB3 code; need analysis on training

--- src/KitchenEnv.py
+++ src/KitchenEnv.py
@@ -4,13 +4,17 @@ from gym import spaces
 import numpy as np
 import pybullet as p
 import pybullet_data  
-
+import time
+from colorama import Fore, Back, Style, init
+init()
 from kitchen import Kitchen
 
+
 class KitchenEnv(gym.Env):
-    def __init__(self, GUI=False):
+    def __init__(self, GUI=False, callback=None):
         super(KitchenEnv, self).__init__()
         self.GUI = GUI
+        self.callback = callback
         if self.GUI: 
             self.client = p.connect(p.GUI)
         else:
@@ -29,28 +33,36 @@ class KitchenEnv(gym.Env):
         # Box spaces for continuous actions and observations
         self.action_space = spaces.Box(low=-1.0, high=1.0, shape=(1,), dtype=np.float32)  # Velocity of the drawer, can be positive (open) or negative (close)
         self.observation_space = spaces.Box(low=np.array([0]), high=np.array([1.8]), dtype=np.float32)  # Angle of the drawer, between 0 and 1.8 radians
-
+        self.velocity_scaling_factor = 0.5  # Scaling factor for the velocity of the drawer
+        self.done = False
         # Initialize the Kitchen
         self.kitchen = Kitchen()
 
     def step(self, action):
+        # time.sleep(5)
         # Execute one time step within the environment
-        reward = 0.0
+        self.done = False # Reset the done flag
         joint_id = self.kitchen.drawer_to_joint_id[1]  # Assuming 1 is the id of the drawer we want to control
-        velocity = action[0]
+        velocity = action[0] * self.velocity_scaling_factor  
+        # print (Fore.CYAN + ("velocity = {}".format(velocity)) + Style.RESET_ALL)
         p.setJointMotorControl2(bodyIndex=self.kitchen.kitchen, jointIndex=joint_id, controlMode=p.VELOCITY_CONTROL, targetVelocity=velocity)
         p.stepSimulation() # used by pybullet to step the simulation
         joint_info = p.getJointState(self.kitchen.kitchen, joint_id)
         joint_position = joint_info[0]
-        if joint_position >= self.kitchen.drawer_to_joint_limits[1][1]:  # If the drawer is open
-            reward = 1.0
-        done = self.check_done()
+        reward, success = self.compute_reward(joint_position, self.kitchen.drawer_to_joint_limits[1][1], self.kitchen.drawer_to_joint_limits[1][0], {})
+        # if joint_position >= self.kitchen.drawer_to_joint_limits[1][1]:  # If the drawer is open
+        #     reward = 1.0
+        done = self.done # check the done after a step
         obs = self.get_observation()
-        if done:
-            print ("action = {} obs = {}  reward = {}  done = {}".format(action, obs, reward, done))
-        return obs, reward, done, {}
+        if done == True:
+            self.reset()
+            # reset the environment
+        return obs, reward, done, {'success': success}
 
     def reset(self):
+        # reset the loggers
+        if self.callback is not None:
+            self.callback.reset()
         # Reset the state of the environment to an initial state
         self.kitchen = Kitchen()
         return self.get_observation()
@@ -65,15 +77,40 @@ class KitchenEnv(gym.Env):
         joint_position = joint_info[0]
         return np.array([joint_position])  # Current angle of the joint
     
-    def check_done(self):
-        joint_info = p.getJointState(self.kitchen.kitchen, self.kitchen.drawer_to_joint_id[1])
-        joint_position = joint_info[0]
-        return joint_position >= self.kitchen.drawer_to_joint_limits[1][1]
+    # def check_done(self):
+    #     joint_info = p.getJointState(self.kitchen.kitchen, self.kitchen.drawer_to_joint_id[1])
+    #     joint_position = joint_info[0]
+    #     goal_position = self.kitchen.drawer_to_joint_limits[1][1]
+    #     print ("joint_position = {} goal_position = {}".format(joint_position, goal_position))
+    #     # time.sleep(0.1)
+    #     return joint_position >= self.kitchen.drawer_to_joint_limits[1][1]
     
     def close(self):
         # Close the environment
         p.disconnect()
-    
+
+    def compute_reward(self, achieved_goal, desired_goal, undesired_goal, info = "sparse"):
+        # Compute the reward given the current and desired goal
+        # print (Fore.BLUE + ("achieved_goal = {} desired_goal = {}".format(achieved_goal, desired_goal)) + Style.RESET_ALL)
+        reward = 0.0
+        success = False
+        if "dense" in info:
+            reward = -np.linalg.norm(achieved_goal - desired_goal, axis=-1)
+        else:
+            if achieved_goal >= desired_goal:
+                reward = 1.0
+                success = True
+                self.done = True # set done to be true: End episode and reach the goal
+                # print (Fore.GREEN + "--------------------------------------------------------"+ Style.RESET_ALL)
+                # print (Fore.GREEN + "----------------Reached the GOAL POSE------------------"+ Style.RESET_ALL)
+                # print (Fore.GREEN + "--------------------------------------------------------"+ Style.RESET_ALL)
+            elif achieved_goal <= undesired_goal: # if the drawer is closed
+                reward = -0.1
+                self.done = True
+            else:
+                reward = 0.0
+        return reward, success
+
 def main():
     # Create an instance of the environment
     env = KitchenEnv()
--- src/sb_train.py
+++ src/sb_train.py
@@ -1,12 +1,57 @@
 from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv, VecNormalize
 from stable_baselines3.common.evaluation import evaluate_policy
-from stable_baselines3.common.callbacks import EvalCallback, CheckpointCallback
-
+from stable_baselines3.common.callbacks import EvalCallback, CheckpointCallback, BaseCallback
+import tensorboard
+import csv
 from KitchenEnv import KitchenEnv
 
+
+class EpisodeEvalCallback(BaseCallback):
+    def __init__(self, eval_env, eval_freq, verbose=0):
+        super(EpisodeEvalCallback, self).__init__(verbose)
+        self.eval_env = eval_env
+        self.eval_freq = eval_freq
+        self.episode_count = 0
+
+    def _on_step(self):
+        if self.locals['dones'][0]:  # Assuming a single environment
+            self.episode_count += 1
+            if self.episode_count % self.eval_freq == 0:
+                mean_reward, _ = evaluate_policy(self.model, self.eval_env, n_eval_episodes=10)
+                print(f"Evaluation result: {mean_reward}")
+        return True
+
+class CSVLoggerCallback(BaseCallback):
+    def __init__(self, csv_file, verbose=0):
+        super(CSVLoggerCallback, self).__init__(verbose)
+        self.csv_file = csv_file
+        self.episode_rewards = 0
+        # self.episode_steps = 0
+        with open(self.csv_file, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(['timesteps', 'total_reward', 'success', 'done'])
+
+    def _on_step(self):
+        self.episode_rewards += self.locals['rewards'][0]
+        # self.episode_steps += 1
+        if self.locals['dones']:
+            success = self.locals['infos'][0].get('success', False)
+            with open(self.csv_file, 'a', newline='') as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerow([self.num_timesteps, self.episode_rewards , success, self.locals['dones']])
+            self.episode_rewards = 0
+            # self.episode_steps = 0
+        return True
+    
+    def reset(self):
+        self.episode_rewards = 0
+        # self.episode_steps = 0
+
+# set the CSV logger callback
+csv_logger_callback = CSVLoggerCallback('./logs/progress.csv')
 # Create environment
-env = KitchenEnv()
+env = KitchenEnv(callback=csv_logger_callback)
 env = DummyVecEnv([lambda: env])  # The algorithms require a vectorized environment to run
 
 # Normalize action and observation space
@@ -14,7 +59,7 @@ env = VecNormalize(env, norm_obs=True, norm_reward=True, clip_obs=10.)
 
 # Create callbacks
 eval_callback = EvalCallback(env, best_model_save_path='./logs/',
-                             log_path='./logs/', eval_freq=500,
+                             log_path='./logs/', eval_freq=1000,
                              deterministic=True, render=False)
 
 checkpoint_callback = CheckpointCallback(save_freq=1000, save_path='./logs/',
@@ -26,7 +71,7 @@ tensorboard_log = "./ppo_kitchen_tensorboard/"
 model = PPO("MlpPolicy", env, verbose=1, tensorboard_log=tensorboard_log)
 
 # Train model
-model.learn(total_timesteps=2500, callback=[eval_callback, checkpoint_callback])
+model.learn(total_timesteps=25000, callback=[eval_callback, checkpoint_callback, csv_logger_callback])
 
 # Save the model
 model.save("ppo_kitchen")
